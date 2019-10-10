acadb9c3376370fb157dc990b01d604a28d57fde
jpuvogel13 <31817732+jpuvogel13@users.noreply.github.com>
2019-10-10 14:27:05 +0800

    Add files via upload

---
+++ week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 1337;
+        int b = 42;
+        int sum= a * b;
+        System.out.println( a + "*" +  b +  "=" + sum);
+        // Program your solution here. Remember to use variables a and b!
+    }
+
+}
