b21e2ae377048b09b5051b96dfeb9994ea6eff52
jpuvogel13 <31817732+jpuvogel13@users.noreply.github.com>
2019-10-11 00:39:32 +0800

    Update Menu.java

--- week4-076.Menu/src/Menu.java
+++ week4-076.Menu/src/Menu.java
@@ -12,11 +12,10 @@ public class Menu {
     // add the methods here
 
     public void addMeal(String meal){
-       if(this.meals.contains(meal)){
-
-       }else
-        this.meals.add(meal);
-    }
+       if(!(this.meals.contains(meal))){
+            this.meals.add(meal);
+       }
+        
     public void printMeals(){
         for(String eat : this.meals){
 
