8c0749b024a46d320d1731a58e655414a65df66b
jpuvogel13 <31817732+jpuvogel13@users.noreply.github.com>
2019-10-11 01:19:25 +0800

    Add files via upload

---
+++ week4-077.LyyraCard/src/LyyraCard.java
@@ -0,0 +1,38 @@
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balanceAtStart){
+        this.balance=balanceAtStart;
+
+    }
+    public String toString(){
+
+        return"The card has " + this.balance + " euros";
+    }
+    public void payEconomical(){
+        if(this.balance<2.50){
+
+        }else
+        this.balance=this.balance-2.50;
+    }
+    public void payGourmet(){
+        if(this.balance<4.00){
+
+        }else
+        this.balance=this.balance-4.00;
+
+
+    }
+    public void loadMoney(double amount){
+        if(amount<0){
+
+        }else if(this.balance+amount>150){
+            this.balance=150.0;
+        }else
+        this.balance+=amount;
+
+    }
+
+
+
+}
