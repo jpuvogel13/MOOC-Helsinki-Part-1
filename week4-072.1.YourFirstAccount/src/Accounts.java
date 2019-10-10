477a00bc7dafa268cacc50ee600dec10735b1d9d
jpuvogel13 <31817732+jpuvogel13@users.noreply.github.com>
2019-10-11 00:26:51 +0800

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+
+        Account justinsAccount = new Account("Justin's account", 100.0);
+        justinsAccount.deposit(20.0);
+        System.out.println(justinsAccount);
+
+    }
+
+}
